2836c5f205337e21b344f9aea0f83d23b011ca34
wangshijun <shijun@arcblock.io>
2018-08-20 10:48:34 +0800

    prepare @arcblock/react-scripts

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -7,51 +7,96 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.dataSources = [
+      {
+        name: 'btc',
+        demoAddress: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa',
+      },
+      {
+        name: 'eth',
+        demoAddress: '0xe65d3128feafd14d472442608daf94bceb91e333',
+      },
+    ];
+
+    this.clients = this.dataSources.reduce((obj, ds) => {
+      obj[ds.name] = new OCAPClient({
+        dataSource: ds.name,
+        httpBaseUrl: 'http://47.104.23.85:8080/api', // for dev in china
+        // httpBaseUrl: 'https://ocap.arcblock.io/api', // for production
+        enableSubscription: true,
+        enableMutation: false,
+      });
+
+      return obj;
+    }, {});
+
     this.state = {
-      address: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa',
+      dataSource: this.dataSources[0],
       summary: null,
+      message: null,
+      timestamp: null,
       loading: false,
     };
 
-    this.client = new OCAPClient({
-      dataSource: 'btc',
-      httpBaseUrl: 'http://47.104.23.85:8080/api', // for dev in china
-      // httpBaseUrl: 'https://ocap.arcblock.io/api', // for production
-      enableSubscription: false,
-      enableMutation: false,
-    });
+    console.log(this.getClient());
+  }
+
+  getClient() {
+    return this.clients[this.state.dataSource.name];
   }
 
   async componentDidMount() {
     this.setState({ loading: true });
 
-    const summary = await this.client.accountByAddress({
-      address: this.state.address,
+    const client = this.getClient();
+
+    const summary = await client.accountByAddress({
+      address: this.state.dataSource.demoAddress,
     });
+
+    // Subscription
+    // const subscription = await client.newBlockMined();
+    // subscription.on('data', data => {
+    //   this.setState({
+    //     message: data,
+    //     timestamp: new Date(),
+    //   });
+    // });
 
     this.setState({ loading: false, summary });
   }
 
   render() {
-    const { loading, summary, address } = this.state;
+    const { loading, summary, dataSource } = this.state;
 
     return (
       <Layout>
-        <h2>OCAP Data Fetching Demo</h2>
+        <h2>Query Demo</h2>
         {loading && (
-          <p>Loading account summary for Bitcoin Address: {address}</p>
+          <p>
+            Loading account summary for {dataSource.name.toUpperCase()} account:{' '}
+            {dataSource.demoAddress}
+          </p>
         )}
         {loading || (
           <div className="App-json">
-            <p>Account summary for Bitcoin account: {address}</p>
+            <p>
+              Account summary for {dataSource.name.toUpperCase()} account:{' '}
+              {dataSource.demoAddress}
+            </p>
             <pre>
               <code>{JSON.stringify(summary, true, '  ')}</code>
             </pre>
           </div>
         )}
+
         <h2>Building your own DApp</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p className="alert">
+          Open BROWSER CONSOLE to view methods provided by OCAPClient.
         </p>
       </Layout>
     );
