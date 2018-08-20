ac3750da1f478c2f11f970cf76459326dc9a90db
wangshijun <shijun@arcblock.io>
2018-08-20 10:23:19 +0800

    prepare @arcblock/react-scripts

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import OCAPClient from '@arcblock/ocap-js';
+import Layout from './components/Layout/';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa',
+      summary: null,
+      loading: false,
+    };
+
+    this.client = new OCAPClient({
+      dataSource: 'btc',
+      httpBaseUrl: 'http://47.104.23.85:8080/api', // for dev in china
+      // httpBaseUrl: 'https://ocap.arcblock.io/api', // for production
+      enableSubscription: false,
+      enableMutation: false,
+    });
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const summary = await this.client.accountByAddress({
+      address: this.state.address,
+    });
+
+    this.setState({ loading: false, summary });
+  }
+
   render() {
+    const { loading, summary, address } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <Layout>
+        <h2>OCAP Data Fetching Demo</h2>
+        {loading && (
+          <p>Loading account summary for Bitcoin Address: {address}</p>
+        )}
+        {loading || (
+          <div className="App-json">
+            <p>Account summary for Bitcoin account: {address}</p>
+            <pre>
+              <code>{JSON.stringify(summary, true, '  ')}</code>
+            </pre>
+          </div>
+        )}
+        <h2>Building your own DApp</h2>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </Layout>
     );
   }
 }
---
+++ packages/react-scripts/template/src/components/Layout/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.png';
+import './layout.css';
+
+export default class Layout extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-links">
+            <AppLink
+              href="https://ocap.arcblock.io"
+              title="SDK Documentation"
+            />
+            <AppLink href="https://ocap.arcblock.io" title="Go Playground" />
+            <AppLink href="https://reactjs.org" title="Learn React" />
+          </div>
+        </header>
+        <div className="App-content">{this.props.children}</div>
+      </div>
+    );
+  }
+}
+
+const AppLink = ({ link, title }) => (
+  <a className="App-link" href={link} target="_blank" rel="noopener noreferrer">
+    {title}
+  </a>
+);
